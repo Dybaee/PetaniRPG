957052245d96ef1a6df62520aac2f5aa3d1a2eaf
Dybaee <daniartha.my@gmail.com>
2024-10-23 17:25:16 +0700

    Fix UI #2

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject settingObj1;
+    public GameObject settingObj2;
+    public GameObject backTO;
+
+    public void OnBackButtonClicked()
+    {
+        settingObj1.SetActive(false);
+        settingObj2.SetActive(false);
+        backTO.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnBackButtonClicked(); 
+        }
+    }
+}
