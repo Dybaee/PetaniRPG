24eda33bbf2966e33cb611222d53ae932d493ffa
Dybaee <daniartha.my@gmail.com>
2024-10-28 11:43:06 +0700

    Add Ingame Paused

---
+++ Assets/Scripts/PausedMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausedMenu : MonoBehaviour
+{
+    public GameObject pauseMenuCanvas; 
+    private bool isPaused = false;
+    public PlayerStateMachine playerScript;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; 
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f; 
+        playerScript.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        pauseMenuCanvas.SetActive(false); 
+        Time.timeScale = 1f;
+        playerScript.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isPaused = false;
+    }
+}
