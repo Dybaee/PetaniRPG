e00e0b6edf1a1d4b6bd121f5865c260bd467f837
Dybaee <daniartha.my@gmail.com>
2024-10-29 11:24:05 +0700

    Adding Save System (Unfix)

--- Assets/Scripts/LoadButtonAnimation.cs
+++ Assets/Scripts/LoadButtonAnimation.cs
@@ -1,33 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoadButtonAnimation : MonoBehaviour
 {
-    public Button loadButton; // Reference to the load button
-    public Animator loadButtonAnimator; // Reference to the button's animator component
-
-    void Start()
-    {
-        // Check if saved data exists
-        if (PlayerPrefs.HasKey("RespawnX"))
-        {
-            // Enable the load button animation if there's saved data
-            loadButtonAnimator.SetBool("HasSave", true);
-            loadButton.interactable = true; // Make the button interactable
-        }
-        else
-        {
-            // Disable the load button animation if no saved data
-            loadButtonAnimator.SetBool("HasSave", false);
-            loadButton.interactable = false; // Make the button non-interactable
-            loadButton.GetComponent<Text>().color = Color.gray; // Set a neutral color for no save
-        }
-    }
-
-    public void LoadGame()
-    {
-        FindObjectOfType<SaveSystem>().LoadSave();
-    }
+    
 }
--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -1,71 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveSystem : MonoBehaviour
 {
-    public Vector3 respawnPoint;
-    public string CheckpointTag = "Checkpoint";
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag(CheckpointTag)) 
-        {
-            respawnPoint = other.transform.position;
-            
-        }
-    }
-
-    void RespawnPlayer()
-    {
-        // Respawn player terakhir cp
-        transform.position = respawnPoint;
-    }
-
-    void PlayerDied()
-    {
-        RespawnPlayer();
-    }
-
-    public void SaveCP()
-    {
-        PlayerPrefs.SetFloat("RespawnX", respawnPoint.x);
-        PlayerPrefs.SetFloat("RespawnY", respawnPoint.y);
-        PlayerPrefs.SetFloat("RespawnZ", respawnPoint.z);
-        PlayerPrefs.Save();
-    }
-
-    public void LoadCP()
-    {
-        
-        if (PlayerPrefs.HasKey("RespawnX"))
-        {
-            float x = PlayerPrefs.GetFloat("RespawnX");
-            float y = PlayerPrefs.GetFloat("RespawnY");
-            float z = PlayerPrefs.GetFloat("RespawnZ");
-            respawnPoint = new Vector3(x, y, z);
-            transform.position = respawnPoint; // Set player terakhir CP
-        }
-        else
-        {
-            respawnPoint = transform.position;
-        }
-    }
-
-    public void LoadSave()
-    {
-        LoadCP();
-    }
+    
 }
