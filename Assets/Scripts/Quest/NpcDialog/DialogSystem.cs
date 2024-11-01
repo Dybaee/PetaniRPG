2cfb2afee3e60e6ac77f3e12668ab949b624e01b
Dybaee <daniartha.my@gmail.com>
2024-11-01 12:51:34 +0700

    Dialog (Unfix)

---
+++ Assets/Scripts/Quest/NpcDialog/DialogSystem.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogSystem : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public GameObject dialogBox;
+    public string[] lines;
+    public float textSpeed;
+    private bool canNextLine;
+    private int index;
+    public MonoBehaviour playerMovementScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogBox.SetActive(true);
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canNextLine)
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                canNextLine = true;
+
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        canNextLine = false;
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            dialogBox.SetActive(false);
+            playerMovementScript.enabled = true;
+        }
+    }
+}
--- Assets/Scripts/Quest/Quest2/NPCTrigger.cs
+++ Assets/Scripts/Quest/Quest2/NPCTrigger.cs
@@ -8,6 +8,12 @@ public class NPCTrigger : MonoBehaviour
 
     private Quest2Ceklist quest2;
     public GameObject fPress;
+    private MonoBehaviour PlayerAttackingState;
+
+    public GameObject dialogSystem;
+    public MonoBehaviour playerMovementScript;
+    public MonoBehaviour dialogueScript;
+    private bool InRange;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +22,29 @@ public class NPCTrigger : MonoBehaviour
         quest2 = GetComponent<Quest2Ceklist>();
     }
 
+    void Update()
+    {
+        if (InRange && Input.GetKeyDown(KeyCode.F))
+        {
+            if (dialogSystem != null)
+            {
+                dialogueScript.enabled = true;
+                bool dialogActive = !dialogSystem.activeSelf;
+                dialogSystem.SetActive(dialogActive);
+
+                PlayerAttackingState.enabled = false;
+                playerMovementScript.enabled = !dialogActive;
+            }
+        }
+    }
+
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            InRange = true;
             fPress.SetActive(true);
-            quest2.Find();
         }
     }
 
@@ -31,6 +53,13 @@ public class NPCTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             fPress.SetActive(false);
+            InRange = false;
+            if (dialogSystem != null && dialogSystem.activeSelf)
+            {
+                dialogSystem.SetActive(false);
+                playerMovementScript.enabled = true;
+                dialogueScript.enabled = false;
+            }
         }
     }
 }
