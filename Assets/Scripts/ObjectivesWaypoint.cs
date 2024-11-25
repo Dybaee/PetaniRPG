35a4faab1ffd6ff2b7a5e56fc3d7cccd0a27b377
Dybaee <daniartha.my@gmail.com>
2024-11-25 20:25:40 +0700

    Disappear Marker

--- Assets/Scripts/ObjectivesWaypoint.cs
+++ Assets/Scripts/ObjectivesWaypoint.cs
@@ -7,37 +7,62 @@ using UnityEngine.UI;
 
 public class ObjectivesWaypoint : MonoBehaviour
 {
-    public Image img;
-    public Transform target;
-    public TextMeshProUGUI meter;
-    public Vector3 offset;
+    public Image img;             
+    public Transform target;       
+    public TextMeshProUGUI meter;  
+    public Vector3 offset;         
+    public float disappearThreshold = 5f;  // jarak dimana waypoint dissapear
 
     private void Update()
     {
-        float minX = img.GetPixelAdjustedRect().width / 2;
-        float maxX = Screen.width - minX;
+        // menghitung jarang antara pemain dan target
+        float distance = Vector3.Distance(target.position, transform.position);
 
-        float minY = img.GetPixelAdjustedRect().height / 2;
-        float maxY = Screen.height - minY;
-
-        Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offset);
-
-        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        // if the target is too close, hide the waypoint
+        if (distance <= disappearThreshold)
         {
-            if (pos.x < Screen.width / 2)
-            {
-                pos.x = maxX;
-            }
-            else
-            {
-                pos.x = minX;
-            }
+            img.gameObject.SetActive(false);  
+            meter.gameObject.SetActive(false); 
+        }
+        else
+        {
+            img.gameObject.SetActive(true);  
+            meter.gameObject.SetActive(true); 
         }
 
-        pos.x = Mathf.Clamp(pos.x, minX, maxX);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        
+        if (img.gameObject.activeSelf)
+        {
+            float minX = img.GetPixelAdjustedRect().width / 2;
+            float maxX = Screen.width - minX;
 
-        img.transform.position = pos;
-        meter.text = Vector3.Distance(target.position, transform.position).ToString("0") + "m";
+            float minY = img.GetPixelAdjustedRect().height / 2;
+            float maxY = Screen.height - minY;
+
+            // mengubah world position menjadi screen position dengan offset
+            Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offset);
+
+            if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+            {
+                if (pos.x < Screen.width / 2)
+                {
+                    pos.x = maxX;
+                }
+                else
+                {
+                    pos.x = minX;
+                }
+            }
+
+            // tetap berada dalam batas layar
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+            // position of the waypoint image
+            img.transform.position = pos;
+
+            // distance text
+            meter.text = distance.ToString("0") + "m";
+        }
     }
 }
